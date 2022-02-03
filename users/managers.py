2c3cca3e1b6400651b8b996edbb1b2648689d829
ali <aliemamalinezhad@gmail.com>
2022-02-03 21:18:03 +0330

    custom user admin

--- users/managers.py
+++ users/managers.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, phone, password, first_name, last_name, **extra_fields):
+    def create_user(self, email, username, phone, first_name, last_name, password=None):
         if not email:
             raise ValueError('User name must have an email')
         if not username:
@@ -14,19 +14,28 @@ class CustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, username=username,
                           phone=phone, first_name=first_name,
-                          last_name=last_name, **extra_fields)
+                          last_name=last_name)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, username, phone, password, first_name, last_name, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_superuser(self, email, username, phone, first_name, last_name, password):
 
-        if extra_fields.get('is_staff') is not True:
+        su_user = self.create_user(email=email, username=username,
+                                   phone=phone, first_name=first_name,
+                                   last_name=last_name, password=password)
+        su_user.is_staff = True
+        su_user.is_superuser = True
+        su_user.is_active = True
+
+        if su_user.is_staff is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
+        if su_user.is_superuser is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, username, phone, password, first_name, last_name, **extra_fields)
+        if su_user.is_active is not True:
+            raise ValueError(_('Superuser must have is_active=True.'))
+
+        su_user.save()
+        return su_user
+
