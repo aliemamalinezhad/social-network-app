11cd5556b48babe9fae12599ba19b0d9472383eb
ali <aliemamalinezhad@gmail.com>
2022-02-04 23:03:21 +0330

    changes on service module

--- users/services.py
+++ users/services.py
@@ -1,9 +1,4 @@
-from rest_framework import generics, permissions, mixins, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
 
 User = get_user_model()
 
